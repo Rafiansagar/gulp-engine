19bbb54516102698f14d2a2747b7d2494da058ae
Rafiansagar <35956615+Rafiansagar@users.noreply.github.com>
2022-08-26 03:42:28 +0600

    push all

---
+++ public_html/assets/js/main.js
@@ -0,0 +1,22 @@
+
+//veriables
+
+/*
+var a, myName , one , two;
+a = "var one <br>";
+myName = "sagar <br>";
+one = "test 1 <br>";
+two = "test 2 <br>";
+
+document.write(a);
+document.write(myName);
+document.write(one);
+document.write(two);
+*/
+
+//arithmetic oparator
+var a , b , result;
+a = 700;
+b = 700;
+result = a + b;
+document.write(result);
